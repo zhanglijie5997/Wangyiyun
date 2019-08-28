c3af019c40c00339dab9686d91174ff2de8c2d5e
toast <yuanyi.he@fun2tech.com>
2019-08-28 16:11:55 +0800

    添加‘朋友’页面样式

--- src/pages/Friends/Friends.tsx
+++ src/pages/Friends/Friends.tsx
@@ -5,13 +5,27 @@ import "./Friends.scss"
 class Friends extends React.Component {
     constructor(props: any) {
         super(props);
+    }
 
+    public showLoginPopove() {
+        console.log(1)
     }
 
     public render() {
         return (
             <div className="friends">
-                findmusic
+                <div className="friends-body">
+                    <div className="friends-body-center">
+                        <div className="friends-center-img">
+                            <div className="friends-center-desc">
+                                你可以关注明星和好友品味他们的私房歌单
+                                <br/>
+                                通过他们的动态发现更多精彩音乐
+                            </div>
+                            <a className="friends-center-btn" onClick={this.showLoginPopove}>立即登录</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
